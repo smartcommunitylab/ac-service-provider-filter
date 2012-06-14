3a38624bbab090e19806b9f1f98a7d67323660ab
zambotti <zambotti@0fd5999a-94d0-4028-93f2-09691c278cb2>
2012-06-14 13:00:47 +0000

    endpointUrl getter and setter; @Value commented

--- src/main/java/eu/trentorise/smartcampus/ac/provider/filters/SpringAcProvider.java
+++ src/main/java/eu/trentorise/smartcampus/ac/provider/filters/SpringAcProvider.java
@@ -17,49 +17,59 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 
 /**
- *
+ * 
  * @author Viktor Pravdin <pravdin@disi.unitn.it>
  * @date Jun 5, 2012 2:36:31 PM
  */
-public class SpringAcProvider implements AuthenticationProvider{
-    
-    @Value("${ac.endpoint.url}")
-    private String endpointUrl;
-    private AcProviderService service;
+public class SpringAcProvider implements AuthenticationProvider {
 
-    @PostConstruct
-    private void init() throws JAXBException {
-        JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
-        factory.setServiceClass(AcProviderService.class);
-        factory.setAddress(endpointUrl);
+	// @Value("${ac.endpoint.url}")
+	private String endpointUrl;
+	private AcProviderService service;
 
-        service = (AcProviderService) factory.create();
+	@PostConstruct
+	private void init() throws JAXBException {
+		JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
+		factory.setServiceClass(AcProviderService.class);
+		factory.setAddress(endpointUrl);
 
-        Client client = ClientProxy.getClient(service);
-        if (client != null) {
-            HTTPConduit conduit = (HTTPConduit) client.getConduit();
-            HTTPClientPolicy policy = new HTTPClientPolicy();
-            policy.setConnectionTimeout(10000);
-            policy.setReceiveTimeout(10000);
-            policy.setAllowChunking(false);
-            conduit.setClient(policy);
-        }
-    }
+		service = (AcProviderService) factory.create();
 
-    @Override
-    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        String token=authentication.getPrincipal().toString();
-        if(!service.isValidUser(token)){
-            throw new BadCredentialsException("Authentication token is absent or expired");
-        }
-        authentication.setAuthenticated(true);
-        return authentication;
-    }
+		Client client = ClientProxy.getClient(service);
+		if (client != null) {
+			HTTPConduit conduit = (HTTPConduit) client.getConduit();
+			HTTPClientPolicy policy = new HTTPClientPolicy();
+			policy.setConnectionTimeout(10000);
+			policy.setReceiveTimeout(10000);
+			policy.setAllowChunking(false);
+			conduit.setClient(policy);
+		}
+	}
 
-    @Override
-    public boolean supports(Class<? extends Object> authentication) {
-        return PreAuthenticatedAuthenticationToken.class.isAssignableFrom(
-                authentication);
-    }
-    
+	@Override
+	public Authentication authenticate(Authentication authentication)
+			throws AuthenticationException {
+		String token = authentication.getPrincipal().toString();
+		if (!service.isValidUser(token)) {
+			throw new BadCredentialsException(
+					"Authentication token is absent or expired");
+		}
+		authentication.setAuthenticated(true);
+		return authentication;
+	}
+
+	@Override
+	public boolean supports(Class<? extends Object> authentication) {
+		return PreAuthenticatedAuthenticationToken.class
+				.isAssignableFrom(authentication);
+	}
+
+	public String getEndpointUrl() {
+		return endpointUrl;
+	}
+
+	public void setEndpointUrl(String endpointUrl) {
+		this.endpointUrl = endpointUrl;
+	}
+
 }
